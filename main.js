5dc2504160401b4c5fbfa7a210b13fdc0e34ce7b
Rubaishaaltaf <160398911+Rubaishaaltaf@users.noreply.github.com>
2024-04-21 14:15:01 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+// Define Variable
+var alienColor = "green";
+// if (alienColor === "green"){
+//     console.log("player just earned 5 points for shooting the alien.")
+// }
+// else{
+//     console.log("player just earned 10 points.");
+// }
+// Second version
+if (alienColor === "red") {
+    console.log("I am come from If statement.");
+}
+else {
+    console.log("I am come from Else statement.");
+}
---
+++ main.ts
@@ -0,0 +1,19 @@
+// Define Variable
+let alienColor = "green";
+
+// First Version
+if (alienColor === "green"){
+    console.log("player just earned 5 points for shooting the alien.")
+}
+else{
+    console.log("player just earned 10 points.");
+}
+
+// Second version
+
+if (alienColor === "red"){
+    console.log("I am come from If statement.");
+}
+else{
+    console.log("I am come from Else statement.");
+}
